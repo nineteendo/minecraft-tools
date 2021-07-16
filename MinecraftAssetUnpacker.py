1bf08c0bee76825735c09f6448ada60dd8f75d0b
Nineteendo <65588599+Nineteendo@users.noreply.github.com>
2021-07-16 16:54:39 +0200

    Now using the json library
    This means now every version is hopefully supported

--- MinecraftAssetUnpacker.py
+++ MinecraftAssetUnpacker.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, json
 
 print("\n\033[95m\033[1mMinecraft Asset Unpacker v1.0.0\n(C) 2021 by Nineteendo\033[0m\n")
 path = os.path.expanduser('~/Library/Application Support/minecraft/assets/')
@@ -6,13 +6,11 @@ path = os.path.expanduser('~/Library/Application Support/minecraft/assets/')
 print("\033[1mOptions:\033[0m " + " ".join(sorted(os.listdir(path + "indexes/"))).replace(".json",""))
 version = input("\033[1mVersion=\033[0m ")
 
-for line in open(path + "indexes/" + version + ".json",'r').readline().replace("}", "{").split("{"):
-    if "." in line:
-        location=version + "/" + line.strip('":, ')
-        if not os.path.isdir(location):
-            os.makedirs(os.path.dirname(location), exist_ok=True)
-    if "hash" in line:
-        hash = line.split(":")[1].split(",")[0].strip('" ')
-        print(location)
-        shutil.copyfile(path + "objects/" + hash[:2] + "/" + hash, path + location)
-input("\nFinished. Press Enter")
+data = json.loads(open(path + "indexes/" + version + ".json",'r').read())["objects"]
+for location in data:
+	print("wrote " + version + "/" + location)
+	if not os.path.isdir(location):
+		os.makedirs(os.path.dirname(path + version + "/" + location), exist_ok=True)
+	hash = data[location]["hash"]
+	shutil.copyfile(path + "objects/" + hash[:2] + "/" + hash, path + version + "/" + location)
+input("\nFinished. Press Enter\n")
